75d4a953a4ad33b27de653157bcc34a3b8e440b2
SurajSingh01 <singhsurajd03@gmail.com>
2020-06-04 21:21:49 +0530

    Tossing Single Coin

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,3 +1,22 @@
 #!/bin/bash -x
 echo "Welcome to flip Coin Simulation"
 
+declare -A singlet
+singlet=([H]=0 [T]=0)
+function single(){
+echo "***************Singlet******************"
+   while [ ${singlet[H]} -ne 15 -a ${singlet[H]} -ne 15 ]
+   do
+      local num1=$((RANDOM%2))
+            if [ $num1 -eq 0 ]
+            then
+         singlet[T]=$((singlet[T] + 1))
+            else
+         singlet[H]=$((singlet[H] + 1))
+            fi
+   done
+echo "${singlet[@]} | total = $((singlet[H] + singlet[T]))"
+echo "H % = $(( (singlet[H] * 100 )/ (singlet[H] + singlet[T]) ))"
+echo "T % = $(( (singlet[T] * 100 )/ (singlet[H] + singlet[T]) ))"
+}
+single
